30170e57057d4b62b9e95671f2db1f59c584f769
Mimi <guzmanm93@gmail.com>
2018-08-13 10:39:28 +1000

    show number message

---
+++ MyFirstForm.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Dim Message As String = "First constant"
+    Dim Number As Integer = 2018
+    Dim Number As Integer = 2008
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        MessageBox.Show(Message)
+        MessageBox.Show(Number)
+        MessageBox.Show(Number)
+    End Sub
+End Class

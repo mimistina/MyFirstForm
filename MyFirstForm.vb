a30d20941c68a659547f522dccec9f1e9bb65e8a
Mimi <guzmanm93@gmail.com>
2018-08-13 10:42:44 +1000

    numebr2 change

--- MyFirstForm.vb
+++ MyFirstForm.vb
@@ -1,10 +1,10 @@
 ﻿Public Class Form1
     Dim Message As String = "First constant"
     Dim Number As Integer = 2018
-    Dim Number As Integer = 2008
+    Dim Number2 As Integer = 2010
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         MessageBox.Show(Message)
         MessageBox.Show(Number)
-        MessageBox.Show(Number)
+        MessageBox.Show(Number2)
     End Sub
 End Class
